2a493868fc6b0bcea192b9196ba9b9e1a2a42cb4
Anselmo Pfeifer <apfeifer@daitangroup.com>
2018-06-09 17:49:52 -0400

    Added new Environment variables

--- packer/createImage.sh
+++ packer/createImage.sh
@@ -10,6 +10,10 @@ elif [ ${OPTION} == 'no' ]; then
    read AWS_ACCESS_KEY
    echo "Enter the AWS_SECRET_ACCESS_KEY"
    read AWS_SECRET_ACCESS_KEY
+   export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY}"
+   export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}"
+   export AWS_DEFAULT_REGION="${REGION}"
+   
    packer build -var aws_access_key="${AWS_ACCESS_KEY}" -var aws_secret_key="${AWS_SECRET_ACCESS_KEY}" template.json
 else
     exit 1
