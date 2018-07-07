0a1e1b98a834632cb85803c1dc94c48cb200f156
Mariano Rodríguez Díaz <contacto@marianord.com>
2018-07-07 01:25:49 -0500

    Fixed script to correct changing minikube ownership.

--- scripts/start-minikube.sh
+++ scripts/start-minikube.sh
@@ -4,16 +4,16 @@
 sudo minikube start --vm-driver=none
 
 # Set Minikube to vagrant instead of root
-sudo mv /root/.kube $HOME/.kube # this will write over any previous configuration
 sudo chown -R $USER $HOME/.kube
 sudo chgrp -R $USER $HOME/.kube
 
-sudo mv /root/.minikube $HOME/.minikube # this will write over any previous configuration
 sudo chown -R $USER $HOME/.minikube
 sudo chgrp -R $USER $HOME/.minikube
 
 # See status
 minikube status
 
-# Start dashboard
+# Start dashboard and prints dashboard URL
 minikube dashboard
+minikube dashboard --url > $DASHBOARDURL
+echo $DASHBOARDURL
